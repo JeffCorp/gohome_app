f7839e03f157c32ce1246dfb8cd83984af1c9059
JeffCorp <patrickjuniorukutegbe@rocketmail.com>
2020-03-12 14:30:02 +0100

    worked on the messaging

---
+++ lib/components/requestComponent.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:go_home/components/pills.dart';
+
+class RequestComponent extends StatelessWidget {
+  final String propTitle;
+  final String location;
+  final String propId;
+  final String imagePath;
+  final int count;
+
+  RequestComponent(
+      {this.propTitle, this.location, this.propId, this.imagePath, this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Card(
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                child: FadeInImage.assetNetwork(
+                  height: 100,
+                  placeholder: "assets/person.png",
+                  image: "http://gohome.ng/assets/upload/" +
+                      propId +
+                      "/" +
+                      imagePath,
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 4,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      propTitle.length > 30
+                          ? propTitle.substring(0, 30) + "..."
+                          : propTitle,
+                      style: TextStyle(
+                          color: Color(
+                            0xFF79c942,
+                          ),
+                          fontSize: 18),
+                    ),
+                    padding: EdgeInsets.only(
+                      top: 10,
+                      bottom: 5,
+                      left: 10,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.only(left: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.location_on),
+                              Text(location),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Pill(count.toString()),
+                              Text("requests"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/notificationService.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../classes/property.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class NotificationServices {
+
+  static const String url = "http://www.gohome.ng/api/get_latest_property_api.php";
+
+
+   static Future<List<Property>> getProperties() async{
+    try {
+      var response;
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+       if(preferences.getBool("isAuth")){
+         String userEmail =preferences.getStringList("user")[1];
+          response =await http.get(url + "?user_email=$userEmail");
+       }else{
+          response =await http.get(url);
+       }
+      if(response.statusCode == 200){
+        List<Property> list = parseProperties(response.body);
+        return list;
+      }else{
+        throw Exception("Error");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  static List<Property> parseProperties(String responseBody){
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Property>((json) => Property.fromJson(json)).toList();
+  }
+
+}
